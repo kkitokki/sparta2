04137354324f543b15b6d0326465c6645cae6f41
kkitokki <kyaxxt@gmail.com>
2022-02-15 18:36:01 +0900

    add isort

--- sparta/urls.py
+++ sparta/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from typing import Dict
+
 from django.contrib import admin
 from django.http import HttpRequest
 from django.urls import path
 from ninja import NinjaAPI
-
 
 api = NinjaAPI()
 
